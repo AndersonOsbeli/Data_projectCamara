8a0ade3e16ff1f13ca83b3658d198d80002374ee
Juan Jose Perez <juanantonio778@gmail.com>
2026-05-24 11:49:43 -0600

    ningun cambio

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SERVER = "localhost"
+SERVER = "DEPINFOR20\SQLEXPRESS"
 DATABASE = "SeniorIA"
 
 connection_string = (
